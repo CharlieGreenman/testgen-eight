af49a7209751f6a8e29e613f079648020572c5a8
Charlie Greenman <charlie@razroo.com>
2023-10-16 15:19:50 -0400

    Add code for angular-16.2.0 add-header-toggle-event.
    [Url: razroo.com/community/angular-16.2.0/side-nav-recipe/add-header-toggle-event]

--- apps/testgen-eight/src/app/app.component.ts
+++ apps/testgen-eight/src/app/app.component.ts
@@ -7,7 +7,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   sideNavOpen = false;
+  sideNavOpen = false;
   title = 'testgen-eight';
+
+  sideNavToggle(): void {
+    this.sideNavOpen = !this.sideNavOpen;
+  }
 
   sideNavToggle(): void {
     this.sideNavOpen = !this.sideNavOpen;
